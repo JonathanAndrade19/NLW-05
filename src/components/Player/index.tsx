990b614bb8fdd7f5c9c9715dcaad8288bf7c21c3
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-04-24 22:10:51 -0300

    new style in new components

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,8 +2,41 @@ import stytes from './styles.module.scss';
 
 export function Player() {
     return (
-        <div>
-            
-       </div>
+        <div className={stytes.playerContainer}>
+            <header>
+                <img src="/playing.svg" alt="Tocando Agora" />
+                <strong>Tocando Agora</strong>
+            </header>
+
+            <div className={stytes.emptyPlayer}>
+                <strong>Selecione um podcast para ouvir</strong>
+            </div>
+
+            <footer className={stytes.empty}>
+                <div className={stytes.progress}>
+                    <span>00:00</span>
+                    <div className={stytes.emptySlider} />
+                    <span>00:00</span>
+                </div>
+
+                <div className={stytes.buttons}>
+                    <button type="button">
+                        <img src="/shuffle.svg" alt="Embaralhar"/>
+                    </button>
+                    <button type="button">
+                        <img src="/play-previous.svg" alt="Tocar anterior"/>
+                    </button>
+                    <button type="button" className={stytes.playButton}>
+                        <img src="/play.svg" alt="Tocar"/>
+                    </button>
+                    <button type="button">
+                        <img src="/play-next.svg" alt="Tocar próxima"/>
+                    </button>
+                    <button type="button">
+                        <img src="/repeat.svg" alt="Repetir"/>
+                    </button>
+                </div>
+            </footer>
+        </div>
     );
 }
